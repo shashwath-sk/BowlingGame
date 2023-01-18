1fc3bb6c95f72fe70a29a1126fe1c7425afda9b3
shashwath sk <shashukarkera01@gmail.com>
2023-01-18 14:50:04 +0530

    chore: add testcases to test bowlingGametracker function

--- javaScript/bowlingGame.test.js
+++ javaScript/bowlingGame.test.js
@@ -1,16 +1,20 @@
 
-const trackScore = require('./bowlingGame');
+const {score , bestScore}= require('./bowlingGameTracker');
 
 
 describe('Bowling game score tracker',()=>{
-    it('Should return TypeError when input is not an array of numbers',()=>
-    {
-        expect(()=>{trackScore}).toThrow(new TypeError('Game should be of ten frames,please continue game to see score'));
-    });
+    
     it('Should return total score when input is an array of numbers',()=>
     {
-        const score = trackScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        expect(score).toBe(16)
-    })
-
+        const gameScore = score([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        expect(gameScore).toEqual(16)
+    });
+    it('Should return TypeError when game is incomplete',()=>
+    {
+        expect(()=>score([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toThrow(new Error('Game should be of ten frames,please continue game to see score'));
+    });
+    it('Should return TypeError when input is not an array of numbers',()=>
+    {
+        expect(()=>score('abc')).toThrow(new Error('Input should be of number Array'));
+    });
 })
